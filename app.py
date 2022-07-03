e6f3da7456a9e79baf938992deead540402d5a41
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-07-03 12:54:40 -0400

    add search endpoint

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static',)
 
 @app.route("/")
 def index():
 	return render_template('index.html')
+
+
+@app.route("/search")
+def search():
+	# print(f"queried for {}")
+	query=request.args.get('query')
+	print(query)
+	return ""
